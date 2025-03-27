269fb525a680837247373b593c51b74d4450a585
JoelJMathewgit <joel.mathew2@ontariotechu.net>
2025-03-27 00:41:56 -0400

    App updated

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,9 @@ import HomePage from './pages/HomePage';
 import MoviesPage from './pages/MoviesPage';
 import FavoritesPage from './pages/FavoritesPage';
 import LoginModal from './components/LoginModal'; // Import Login Modal
-import SignupModal from './components/SignupModal'; // Import Signup Modal
+import SignupModal from './components/SIgnupModal'; // Import Signup Modal
+import { signInWithEmailAndPassword } from 'firebase/auth';
+import { auth } from './firebase'; 
 import './styles.css';
 
 function App() {
@@ -15,24 +17,31 @@ function App() {
   const [loginUsername, setLoginUsername] = useState('');
   const [loginPassword, setLoginPassword] = useState('');
 
-  const handleLoginSubmit = (e) => {
+  const handleLoginSubmit = async (e) => {
     e.preventDefault();
-    setLoggedInUser(loginUsername); // Simulated login
-    setShowLogin(false);
-    setLoginUsername('');
-    setLoginPassword('');
+    try {
+      const userCredential = await signInWithEmailAndPassword(auth, loginUsername, loginPassword);
+      const user = userCredential.user;
+
+      setLoggedInUser(user.email); 
+      setShowLogin(false); 
+      setLoginUsername('');
+      setLoginPassword('');
+    } catch (error) {
+      console.error(error.message); 
+    }
   };
 
   const handleLogout = () => {
-    setLoggedInUser(null);
+    setLoggedInUser(null); 
   };
 
   return (
     <Router>
-      <Header loggedInUser={loggedInUser} setShowLogin={setShowLogin} setShowSignup={setShowSignup} handleLogout={handleLogout}/>
+      <Header loggedInUser={loggedInUser} setShowLogin={setShowLogin} setShowSignup={setShowSignup} handleLogout={handleLogout} />
 
       <Routes>
-        <Route path="/" element={<HomePage />} /> 
+        <Route path="/" element={<HomePage />} />
         <Route path="/movies" element={<MoviesPage />} />
         <Route path="/favorites" element={<FavoritesPage />} />
       </Routes>
@@ -46,6 +55,7 @@ function App() {
           setLoginPassword={setLoginPassword}
           handleLoginSubmit={handleLoginSubmit}
           setShowLogin={setShowLogin}
+          setLoggedInUser={setLoggedInUser} 
         />
       )}
       {showSignup && (
