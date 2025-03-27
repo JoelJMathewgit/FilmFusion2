d8efafd8b11d3ec7fa5f46a6526842a9c16e093d
shahsyed4 <shah.syed1@ontariotechu.net>
2025-03-27 00:16:49 -0400

    basic FavoritePage added for test case

---
+++ src/pages/FavoritesPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function FavoritesPage() {
+  return (
+    <div className="container">
+      <h1 className="title">Favorite Movies</h1>
+      <p>Your favorite movies will appear here.</p>
+    </div>
+  );
+}
+
+export default FavoritesPage;
